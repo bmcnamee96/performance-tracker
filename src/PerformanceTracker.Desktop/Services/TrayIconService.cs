d4a953bb4679a03ad81770a1d1bb0830a20fd54f
bmcnamee96 <bmac965@gmail.com>
2026-06-22 20:44:41 -0400

    Add branded desktop app icon

--- src/PerformanceTracker.Desktop/Services/TrayIconService.cs
+++ src/PerformanceTracker.Desktop/Services/TrayIconService.cs
@@ -1,9 +1,12 @@
+using System.IO;
+using System.Windows.Resources;
 using Forms = System.Windows.Forms;
 
 namespace PerformanceTracker.Desktop.Services;
 
 public sealed class TrayIconService : IDisposable
 {
+    private static readonly Uri AppIconUri = new("pack://application:,,,/Assets/performance-tracker.ico", UriKind.Absolute);
     private Forms.NotifyIcon? _notifyIcon;
 
     public void Initialize(Action showWindow, Action exitApplication)
@@ -19,7 +22,7 @@ public sealed class TrayIconService : IDisposable
 
         _notifyIcon = new Forms.NotifyIcon
         {
-            Icon = global::System.Drawing.SystemIcons.Application,
+            Icon = CreateAppIcon(),
             Text = "Performance Tracker",
             Visible = true,
             ContextMenuStrip = menu
@@ -38,5 +41,22 @@ public sealed class TrayIconService : IDisposable
         _notifyIcon.Visible = false;
         _notifyIcon.Dispose();
         _notifyIcon = null;
+    }
+
+    private static global::System.Drawing.Icon CreateAppIcon()
+    {
+        StreamResourceInfo? iconResource = System.Windows.Application.GetResourceStream(AppIconUri);
+        if (iconResource is null)
+        {
+            return global::System.Drawing.SystemIcons.Application;
+        }
+
+        using Stream resourceStream = iconResource.Stream;
+        using MemoryStream iconStream = new();
+        resourceStream.CopyTo(iconStream);
+        iconStream.Position = 0;
+
+        using global::System.Drawing.Icon icon = new(iconStream);
+        return (global::System.Drawing.Icon)icon.Clone();
     }
 }
